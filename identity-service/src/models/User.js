0f7ab1de232e034928af32913990d8487d6222b8
christopher18-cyber <christopheradegoke4@gmail.com>
2025-12-15 13:18:26 +0100

    reapply gitignore rules and remove env files

--- identity-service/src/models/User.js
+++ identity-service/src/models/User.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true })
 
+
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
         try { }
