034430c3cfed355a75d5026e56df9e293493b995
christopher18-cyber <christopheradegoke4@gmail.com>
2025-12-20 10:51:26 +0100

    did the login and refresh token part

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,5 +1,5 @@
 import logger from "../utils/logger.js";
-import { validateRegistration } from "../utils/validation.js";
+import { validateLogin, validateRegistration } from "../utils/validation.js";
 import User from "../models/User.js";
 import { generateTokens } from "../utils/generateToken.js";
 
@@ -47,6 +47,78 @@ export async function registerUser(req, res) {
         logger.error("Registeration error occured.", err)
         res.status(500).json({
             message: `Internal server error.`,
+            success: false
+        })
+    }
+}
+
+// login an user
+
+export async function loginUser(req, res) {
+    logger.info("Login endpoint hit.....")
+    try {
+        const { error } = validateLogin(req.body)
+        if (error) {
+            logger.warn("Validation error", error.details[0].message)
+            res.status(400).json({
+                success: false,
+                message: error.details[0].message
+            })
+        } else {
+            const { email, password } = req.body
+            const user = await User.findOne({ email })
+            if (!user) {
+                logger.warn("Invalid user")
+                return res.status(400).json({
+                    success: false,
+                    message: "Invalid credentials."
+                })
+            } else {
+                const isValidPassword = await user.comparePassword(password)
+                if (!isValidPassword) {
+                    logger.warn("Invalid password")
+                    res.status(404).json({
+                        success: false,
+                        message: "Invalid password."
+                    })
+                } else {
+                    const { accessToken, refreshToken } = await generateTokens(user)
+
+                    res.json({
+                        accessToken,
+                        refreshToken,
+                        userId: user._id
+                    })
+                }
+            }
+        }
+    } catch (error) {
+        logger.error("Login error occured.", err)
+        res.status(500).json({
+            message: `Internal server error.`,
+            success: false
+        })
+    }
+}
+
+// refresh token
+
+export async function refreshToken(req, res) {
+    logger.info("Refresh token endpoint hit")
+    try {
+        const { refreshToken } = req.body
+        if (!refreshToken) {
+            logger.warn("Refresh token missing.")
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials."
+            })
+        }
+    }
+    catch (err) {
+        logger.error("Refresh token error occured.")
+        res.status(500).json({
+            message: "Internal server error",
             success: false
         })
     }
--- identity-service/src/utils/validation.js
+++ identity-service/src/utils/validation.js
@@ -10,3 +10,13 @@ export function validateRegistration(data) {
 
     return schema.validate(data)
 }
+
+export function validateLogin(data) {
+    const schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(6).required()
+    })
+
+
+    return schema.validate(data)
+}
